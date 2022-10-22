fe06ef1e3b4a68039b86e4823e1b044a7db3156d
Uzuhi <90253690+Uzuhi@users.noreply.github.com>
2022-10-22 14:43:10 +0300

    Update Point3d.java

--- Point3d.java
+++ Point3d.java
@@ -4,42 +4,30 @@ import java.lang.Math;
 
 public class Point3d extends Class2d{
 	Scanner s = new Scanner(System.in);
-	private double xCoord;					//координата x
-	private double yCoord;					//координата y
-	private double zCoord;					//координата z
+	private double zCoord;							//координата z
+	public Point3d(double x, double y, double z) {						
+		super(x, y);
+		zCoord = z;
+	}
 	public Point3d() {						//создание нового объекта со значениями (0.0, 0.0, 0.0)
-		xCoord = 0.0;
-		yCoord = 0.0;
-		zCoord = 0.0;
-	}
-	public double getX() {					//возможность получения 3 значений по отдельности
-		return xCoord;
-	}
-	public double getY() {
-		return yCoord;
+		this (0.0, 0.0, 0.0);
 	}
 	public double getZ() {
 		return zCoord;
 	}
-	public void setX(double value) {		//возможность изменения 3 значений по отдельности
-		xCoord = value;
-	}
-	public void setY(double value) {
-		yCoord = value;
-	}
 	public void setZ(double value) {
 		zCoord = value;
 	}
-	public boolean compare() {			//метод для сравнения значений объектов
-		if ((xCoord == zCoord) && (yCoord == xCoord) && (zCoord == yCoord))
+	public boolean compare(Point3d point) {			//метод для сравнения значений объектов
+		if ((super.compare(point)) && (zCoord == point.getZ()))
 			return true;
 		else
 			return false;
-	}
+	}//сравнение точек, не координат
 	public double distanceTo(Point3d point) {			//вычисление расстояния между двумя точками с точностью до 2 знаков после запятой
 		double X, Y, Z;
-        X = xCoord - point.getX();
-        Y = yCoord - point.getY();
+        X = this.getX() - point.getX();
+        Y = this.getY() - point.getY();
         Z = zCoord - point.getZ();
         return Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2) + Math.pow(Z, 2));
 	}
